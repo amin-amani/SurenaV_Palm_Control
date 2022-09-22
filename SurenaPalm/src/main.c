598b076535304e949536f34e61eab97cb9f21109
A.Amani <didi1364@gmail.com>
2022-09-22 14:23:12 +0330

    feat: SPI2 initialization completed

--- SurenaPalm/src/main.c
+++ SurenaPalm/src/main.c
@@ -5,6 +5,8 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/gpio.h"
+#include "esp_system.h"
+#include "driver/spi_master.h"
 #include "sdkconfig.h"
 #include "driver/uart.h"
 
@@ -12,28 +14,64 @@
 #define GPIO_INPUT_IO_0     12
 #define GPIO_INPUT_IO_1     14
 #define GPIO_INPUT_PIN_SEL  ((1ULL<<GPIO_INPUT_IO_0) | (1ULL<<GPIO_INPUT_IO_1))
+
+
+//clk 14 D25
+//miso 12 D32
+//mosi 13 D33
+//cs 15  D26
+
+#define BMP_SCK 14
+#define BMP_MISO 12
+#define BMP_MOSI 13
+#define BMP_CS 15
+
+// from sites
+// #define BMP_SCK 13
+// #define BMP_MISO 12
+// #define BMP_MOSI 11
+// #define BMP_CS 10
+
+
 //====================================================================================================================
 void GPIOInit()
 {
-        gpio_config_t io_conf = {};
-   //interrupt of rising edge
-    io_conf.intr_type = GPIO_INTR_DISABLE;
-    //bit mask of the pins, use GPIO4/5 here
-    io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
-    //set as input mode
-    io_conf.mode = GPIO_MODE_INPUT;
-    //enable pull-up mode
-    io_conf.pull_up_en = 1;
-    gpio_config(&io_conf);
       
     gpio_pad_select_gpio(BLINK_GPIO);
     /* Set the GPIO as a push/pull output */
     gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
 
 }
+//====================================================================================================================
 void app_main() 
 {
 GPIOInit();
+esp_err_t ret;
+    spi_device_handle_t spi;
+    spi_bus_config_t buscfg={
+        .miso_io_num=BMP_MISO,
+        .mosi_io_num=BMP_MOSI,
+        .sclk_io_num=BMP_SCK,
+        .quadwp_io_num=-1,
+        .quadhd_io_num=-1,
+        .max_transfer_sz=8
+    };
+    spi_device_interface_config_t devcfg={
+
+        .clock_speed_hz=10*1000*1000,           //Clock out at 10 MHz
+        .mode=0,                                //SPI mode 0
+        .spics_io_num=BMP_CS,               //CS pin
+        .queue_size=7,                          //We want to be able to queue 7 transactions at a time
+        // .pre_cb=lcd_spi_pre_transfer_callback,  //Specify pre-transfer callback to handle D/C line
+    };
+
+    printf("hello world\n");
+    ret=spi_bus_initialize(SPI2_HOST, &buscfg, SPI_DMA_CH_AUTO);
+    ESP_ERROR_CHECK(ret);
+    printf("spi_bus_initialize= %d\n",ret);
+    ret=spi_bus_add_device(SPI2_HOST, &devcfg, &spi);
+    ESP_ERROR_CHECK(ret);
+    printf("spi_bus_add_device= %d\n",ret);
 while (1)
 {
     /* code */  
@@ -42,7 +80,5 @@ while (1)
     gpio_set_level(BLINK_GPIO, 1);
     vTaskDelay(500 / portTICK_PERIOD_MS);
 }
-
-
 
 }
